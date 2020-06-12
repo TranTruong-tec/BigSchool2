ff5e7fb3c22c06aecd307928240d8f07c150b1ab
Administrator <Administrator@PC203>
2020-06-12 15:17:16 +0700

    Add project files.

---
+++ bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using bigschool.Models;
+using bigschool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbcontext;
+        // GET: Courses
+        public CoursesController()
+        {
+            _dbcontext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbcontext.Categories.ToList()
+            };
+
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbcontext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbcontext.Courses.Add(course);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
